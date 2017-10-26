c0ae39ccf7300c5009dbd46bfb19428f5bdd8eec
hemant_revankar <hemantkrevankar@gmail.com>
2017-10-26 23:19:22 +0530

    Added more methods to the Graph class

--- Graph/src/com/hemant/datastructure/graph/Graph.java
+++ Graph/src/com/hemant/datastructure/graph/Graph.java
@@ -6,10 +6,12 @@ import java.util.Queue;
 public class Graph {
 	private LinkedList<Integer> adjacencyList[];
 	private Integer v;
+	private Integer edges;
 
 	@SuppressWarnings("unchecked")
 	public Graph(Integer vertices) {
 		this.v = vertices;
+		this.edges = 0;
 		adjacencyList = new LinkedList[vertices];
 		
 		for (int i = 0; i < v; i++) {
@@ -20,24 +22,46 @@ public class Graph {
 	public void addEdge(Integer source, Integer destination) {
 		adjacencyList[source].add(destination);
 		adjacencyList[destination].add(source);
+		this.edges++;
 	}
 	
 	public void removeEdge(Integer source, Integer destination) {
 		adjacencyList[source].remove(destination);
 		adjacencyList[destination].remove(source);
+		this.edges--;
 	}
 
-	public void printGraph() {
+	public String printGraph() {
+		StringBuilder s = new StringBuilder();
+		s.append("Vertices: " + this.v + " and Edges: " + this.edges + "\n");
 		for (int i = 0; i < v; i++) {
-			System.out.println("Adjacency list for vertex " + i);
+			s.append(i + ":");
 			for (int j : adjacencyList[i]) {
-				System.out.println(j);
+				s.append(j + " ");
 			}
+			s.append("\n");
 		}
+		return s.toString();
 	}
 	
+	public LinkedList<Integer> adjacentVertices(Integer vertex) {
+		return adjacencyList[vertex];
+	}
+
+	public Integer degree(Integer vertex) {
+		return adjacencyList[vertex].size();
+	}
+
+	public Integer vertices() {
+		return this.v;
+	}
+
+	public Integer edges() {
+		return edges;
+	}
+
 	public void breadthFirstSearch() {
-		Queue<Integer> queue = new 
+		//Queue<Integer> queue = new 
 	}
 }
 
--- Graph/src/com/hemant/datastructure/graph/Test.java
+++ Graph/src/com/hemant/datastructure/graph/Test.java
@@ -2,14 +2,22 @@ package com.hemant.datastructure.graph;
 
 public class Test {
 	public static void main(String[] args) {
-		Graph undirectedGraph = new Graph(5);
+		Graph undirectedGraph = new Graph(13);
 		undirectedGraph.addEdge(0, 1);
 		undirectedGraph.addEdge(0, 2);
-		undirectedGraph.addEdge(1, 3);
-		undirectedGraph.addEdge(2, 4);
+		undirectedGraph.addEdge(0, 6);
+		undirectedGraph.addEdge(0, 5);
+		undirectedGraph.addEdge(3, 5);
+		undirectedGraph.addEdge(3, 4);
+		undirectedGraph.addEdge(4, 5);
+		undirectedGraph.addEdge(4, 6);
+		undirectedGraph.addEdge(7, 8);
+		undirectedGraph.addEdge(9, 10);
+		undirectedGraph.addEdge(9, 11);
+		undirectedGraph.addEdge(9, 12);
+		undirectedGraph.addEdge(11, 12);
 		
 		undirectedGraph.printGraph();
-		undirectedGraph.removeEdge(0, 2);
-		undirectedGraph.printGraph();
+		System.out.println(undirectedGraph.printGraph());
 	}
 }
